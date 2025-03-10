9d62cb1539b4b52abf3e458fb037cb75f52320a2
anyaachan <anna.danchenko@icloud.com>
2025-03-10 15:58:31 +0100

    configure generation to support schema

--- src/utils/generation.py
+++ src/utils/generation.py
@@ -2,7 +2,9 @@
 import requests
 from src.config import OPENROUTER_API_KEY
 
-def call_llm_openrouter(prompt:str, model_name:str, json_schema:dict=None) -> str:
+def call_llm_openrouter(prompt:str, 
+                        model_name:str,
+                        json_schema:dict=None) -> str:
     """
     Call the OpenRouter API to generate text from a prompt using given model and optional schema.
     """
@@ -10,38 +12,36 @@ def call_llm_openrouter(prompt:str, model_name:str, json_schema:dict=None) -> st
         "Authorization": f"Bearer {OPENROUTER_API_KEY}",
         "Content-Type": "application/json",
     }
-    
     properties = {}
-    for prop_name, description in json_schema.items():
-        properties[prop_name] = {
-            "type": "string",
-            "description": description
-        }
-    
-    if json_schema is None:
-        payload = {
+
+    payload = {
         "model": model_name,
         "messages": [{"role": "user", "content": prompt}]
         }
-    else:
-        payload = {
-            "model": model_name,
-            "messages": [{"role": "user", "content": prompt}],
-            "response_format": {
-                "type": "json_schema",
-                "json_schema": {
-                    "name": "answer",
-                    "strict": "true",
-                    "schema": {
-                        "type": "object",
-                        "properties": properties,
-                        "required": ["shouldRespond"],
-                        "additionalProperties": "false"
-                        }
+            
+    if json_schema is not None:
+        for prop_name, description in json_schema.items():
+            properties[prop_name] = {
+                "type": "string",
+                "description": description
+            }
+            
+        payload["response_format"] = {
+            "type": "json_schema",
+            "json_schema": {
+                "name": "answer",
+                "strict": True,
+                "schema": {
+                    "type": "object",
+                    "properties": properties,
+                    "required": list(properties.keys()),
+                    "additionalProperties": False
                     }
                 }
             }
     
+    print(list(properties.keys()))
+    print(payload)
     response = requests.post(
         "https://openrouter.ai/api/v1/chat/completions", 
         headers=headers, 
@@ -49,4 +49,7 @@ def call_llm_openrouter(prompt:str, model_name:str, json_schema:dict=None) -> st
         )
     response.raise_for_status()
     
-    return response.json()["choices"][0]["message"]["content"]    
+    if json_schema is None:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        return response.json()
