d52b7de3e11a5eee877c80d24c65f0e8f9ae71e4
anyaachan <anna.danchenko@icloud.com>
2025-03-11 12:32:17 +0100

    load embeddings once by passing it in main

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from src.utils.embeddings import get_embedding_model
 from src.data.indexing import load_documents, create_vector_store
 from src.data.preprocessing import preprocess_csv
 from src.chatbot import RecipeChatbot
-from src.utils.text_processing import clean_html_tags, remove_hashtags, split_steps
+from src.utils.generation import call_llm_openrouter
 
 def preprocess_data(input_file_path, output_file_path):
     """
     Preprocess the csv file with data and save to csv.
-    More about steps can be found in explanatory notebook "01_data_preprocessing.ipynb".
     """
     print("Loading and preprocessing data...")
     df = pd.read_csv(input_file_path)
@@ -21,14 +20,14 @@ def index_data(embeddings):
     Index the data and store in Chroma database.
     """
     rag_path = os.path.join(PROCESSED_DATA_DIR, "Recipes_processed_rag.csv")
-    docs = load_documents(rag_path)
+    docs = load_documents(rag_path, ["id", "author_note"])
     create_vector_store(docs, embeddings)
 
-def start_chatbot():
+def start_chatbot(embeddings):
     """
     Start the chatbot in CLI. 
     """
-    chatbot = RecipeChatbot()
+    chatbot = RecipeChatbot(embeddings)
     
     print("\nWelcome to the Recipe Chatbot!")
     print("Type 'exit' to end the conversation.")
@@ -41,16 +40,18 @@ def start_chatbot():
             break
         
         response = chatbot.chat(user_input)
-        print(f"\nChatbot: {response}")
+        print(f"\nChatbot: {response["answer"]}")
 
 def main():
+    # testing
+    # print(call_llm_openrouter("What is the capital of France?", "google/gemini-2.0-flash-001", {"answer": "Answer"}))
     embeddings = get_embedding_model() # loading embedding here to avoid multiple loads
     preprocess_data(
         os.path.join(INPUT_DATA_DIR, "Recipes.csv"), 
         os.path.join(PROCESSED_DATA_DIR, "Recipes_processed_rag.csv")
         )
     index_data(embeddings)
-    start_chatbot()
+    start_chatbot(embeddings)
 
 if __name__ == "__main__":
     main()
