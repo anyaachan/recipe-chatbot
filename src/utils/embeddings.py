ba3bdd4bc390dd15fa62d0b108e117123a8518e8
anyaachan <anna.danchenko@icloud.com>
2025-03-11 02:13:40 +0100

    add func to get all chroma docs

--- src/utils/embeddings.py
+++ src/utils/embeddings.py
@@ -1,6 +1,7 @@
 import os
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from src.config import MODELS_DIR, DEFAULT_EMBEDDING_MODEL
+from src.config import MODELS_DIR, DEFAULT_EMBEDDING_MODEL, CHROMA_DB_PATH
+from langchain_community.vectorstores import Chroma
 
 def get_embedding_model(model_name=DEFAULT_EMBEDDING_MODEL, cache_dir=MODELS_DIR):
     """
@@ -16,3 +17,21 @@ def get_embedding_model(model_name=DEFAULT_EMBEDDING_MODEL, cache_dir=MODELS_DIR
     
     print(f"Embedding model ready to use.")
     return embeddings
+
+def get_all_chroma_embeddings(embedding_model):
+    """
+    Load all available Chroma embeddings and documents.
+    """
+    vectorstore = Chroma(
+        persist_directory=CHROMA_DB_PATH,
+        embedding_function=embedding_model
+    )
+    
+    all_documents = vectorstore.get(include=['embeddings', 'metadatas', 'documents'])
+    
+    return {
+        'embeddings': all_documents['embeddings'],
+        'documents': all_documents['documents'],
+        'metadatas': all_documents['metadatas'],
+        'ids': all_documents['ids']
+    }
