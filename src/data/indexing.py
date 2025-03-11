03086f956a404019b7079a30f0de26b9f2dd5923
anyaachan <anna.danchenko@icloud.com>
2025-03-11 12:31:17 +0100

    add print statements and ability to put cols into metadata

--- src/data/indexing.py
+++ src/data/indexing.py
@@ -5,17 +5,24 @@ from langchain_community.vectorstores import Chroma
 from src.config import CHROMA_DB_PATH
 from src.utils.text_processing import find_max_len_doc
 
-def load_documents(csv_path: str) -> list:
+def load_documents(csv_path: str, metadata_columns: list = None) -> list:
     """ 
     Process csv into documents. Print maximum document length.
     """
-    loader = CSVLoader(file_path=csv_path)
+    if metadata_columns is None:
+        loader = CSVLoader(file_path=csv_path)
+    else:
+        loader = CSVLoader(file_path=csv_path, metadata_columns=metadata_columns)
+        
     docs = loader.load()
     
     max_length, max_index = find_max_len_doc(docs)
     print(f"Loaded {len(docs)} documents")
     print(f"Maximum document length: {max_length} characters")
     print(f"Found in document index: {max_index}")
+    print("=====================")
+    print("Document with max index contents: ")
+    print(docs[max_index].page_content)
     
     return docs
 
@@ -42,6 +49,7 @@ def load_vector_store(embedding_model, db_path=CHROMA_DB_PATH):
     if not os.path.exists(db_path):
         raise FileNotFoundError(f"Vector store not found at {db_path}")
     
+    # use langchains chroma to further convience
     vectorstore = Chroma(
         persist_directory=db_path,
         embedding_function=embedding_model
